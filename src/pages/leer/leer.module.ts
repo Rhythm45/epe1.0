0a7221a8757c3453b0f4a84ee807aac9a669bc01
SacroMetal <jeresn@gmail.com>
2018-05-28 01:05:22 -0400

    beneficios de lectura

---
+++ src/pages/leer/leer.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { LeerPage } from './leer';
+
+@NgModule({
+  declarations: [
+    LeerPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(LeerPage),
+  ],
+})
+export class LeerPageModule {}
